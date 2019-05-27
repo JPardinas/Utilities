2720ea6091296e58383d537d6892f11b2b8ccd0b
Juan Pardiñas Bárbara <1707652-JPardinas@users.noreply.gitlab.com>
2019-05-27 13:43:07 +0000

    SearchFiles class

---
+++ Files & Streams - Data/General/SearchFiles/java/Inicio.java
@@ -0,0 +1,306 @@
+import java.io.File;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+
+
+
+public class Inicio {
+	
+	private Display display;
+	private Shell shell;
+	private Text labelResult;
+	private Text textFilePath;
+	private Text textTextToSearch;
+	private Text textNewFilePath;
+	private Button buttonFileNames;
+	private Button buttonTextInTo;
+	private Integer searchMode;
+	private Text labelErrors;
+	
+	public Inicio () {
+		initialize();
+		open();
+	}
+	
+
+
+	private void initialize() {
+		display = new Display();
+		shell = new Shell(display);
+		searchMode = -1;
+		
+		configureShell();
+		createComposite();
+		
+	}
+	
+	private void configureShell() {
+		
+		shell.setSize(500,300);
+		shell.setMinimumSize(300, 300);
+		shell.setLayout(new GridLayout(1, false));
+		shell.setText("File Searcher");
+		
+		Rectangle screenSize = display.getPrimaryMonitor().getBounds();
+		shell.setLocation((screenSize.width - shell.getBounds().width) / 2, (screenSize.height - shell.getBounds().height) / 2);
+	}
+
+
+
+	
+	private void createComposite() {
+		
+		final Composite composite = new Composite(shell, SWT.FILL);
+	    composite.setLayout(new GridLayout(1, false));
+		composite.setLayoutData(new GridData(GridData.FILL,
+				GridData.BEGINNING, true, false));
+		
+		Group groupFiltrosGenerales = new Group(composite, SWT.NONE);
+		groupFiltrosGenerales.setLayout(new GridLayout(2, false));
+		groupFiltrosGenerales.setLayoutData(new GridData(GridData.FILL,
+				GridData.FILL, true, false));
+		groupFiltrosGenerales.setText("Directory");
+		
+		
+		
+		
+		createFirstRow(groupFiltrosGenerales);
+		createSecondRow(groupFiltrosGenerales);
+		createThirdRow(groupFiltrosGenerales);
+		createFourthRow(groupFiltrosGenerales);
+		createButton(composite);
+		
+	}
+
+
+
+
+	private void createFirstRow(Group groupFiltrosGenerales) {
+		
+		Label labelFile = new Label(groupFiltrosGenerales, SWT.FILL);
+		GridData gridDataLabelId = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
+		labelFile.setLayoutData(gridDataLabelId);
+		labelFile.setText("Files Path: ");
+		
+		
+		textFilePath = new Text(groupFiltrosGenerales, SWT.SINGLE | SWT.BORDER);
+		GridData gdText = new GridData(4, 2, true, false);
+		gdText.widthHint = 500;
+		textFilePath.setLayoutData(gdText);
+		textFilePath.setText("");
+		
+	}
+	
+	
+	
+	private void createSecondRow(Group groupFiltrosGenerales) {
+		
+		Label labelFile = new Label(groupFiltrosGenerales, SWT.FILL);
+		GridData gridDataLabelId = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
+		labelFile.setLayoutData(gridDataLabelId);
+		labelFile.setText("Text to Search: ");
+		
+		
+		textTextToSearch = new Text(groupFiltrosGenerales, SWT.SINGLE | SWT.BORDER);
+		GridData gdText = new GridData(4, 2, true, false);
+		gdText.widthHint = 500;
+		textTextToSearch.setLayoutData(gdText);
+		textTextToSearch.setText("");
+		
+	}
+	
+	
+	private void createThirdRow(Group groupFiltrosGenerales) {
+		
+		Label labelFile = new Label(groupFiltrosGenerales, SWT.FILL);
+		GridData gridDataLabelId = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
+		labelFile.setLayoutData(gridDataLabelId);
+		labelFile.setText("New File Path: ");
+		
+		
+		textNewFilePath = new Text(groupFiltrosGenerales, SWT.SINGLE | SWT.BORDER);
+		GridData gdText = new GridData(4, 2, true, false);
+		gdText.widthHint = 500;
+		textNewFilePath.setLayoutData(gdText);
+		textNewFilePath.setText("");
+		
+	}
+	
+	
+	private void createFourthRow(Group groupFiltrosGenerales) {
+		
+		Label labelFile = new Label(groupFiltrosGenerales, SWT.FILL);
+		GridData gridDataLabelId = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
+		labelFile.setLayoutData(gridDataLabelId);
+		labelFile.setText("Search Mode: ");
+		
+		
+		Composite genderGroup = new Composite(groupFiltrosGenerales, SWT.NONE);
+		genderGroup.setLayout(new GridLayout(2, false));
+		 
+		 
+		buttonFileNames = new Button(genderGroup, SWT.RADIO);
+		buttonFileNames.setText("File Names");
+		buttonFileNames.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				searchMode = 0;
+				
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		 
+		buttonTextInTo = new Button(genderGroup, SWT.RADIO);
+		buttonTextInTo.setText("Text in to Files");
+		buttonTextInTo.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				searchMode = 1;
+				
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+	}
+	
+	
+	
+	private void createButton(Composite composite) {
+		
+		Button button = new Button(composite, SWT.CENTER);
+		GridData gridDataLabelId = new GridData(SWT.RIGHT, SWT.NONE, false, false);
+		gridDataLabelId.widthHint = 100;
+		button.setLayoutData(gridDataLabelId);
+		button.setText("Start");
+		
+		button.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				
+				if (validValues()) {
+					
+					File testPath = new File (textFilePath.getText());
+					File testCopyPath = new File (textNewFilePath.getText());
+					
+					if (!testPath.exists()) {
+						labelResult.setText("File Path not found");
+					} else if (!testCopyPath.exists()) {
+						labelResult.setText("Copy File Path not found");
+					} else {
+						List<String> result = SearchFile.searchForFiles(textTextToSearch.getText(), textFilePath.getText(), textNewFilePath.getText(), searchMode);
+						labelResult.setText("Total Files (" + result.size() + "): ");
+						for (String resultString: result) {
+							labelResult.setText(labelResult.getText().concat(resultString).concat(", "));
+						}
+						
+						labelErrors.setText("Total Errors: " + SearchFile.getNumErrors() + "     ");
+					}
+					
+					
+					
+					
+				} else {
+					labelResult.setText("Values not valid, check it");
+				}
+				
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		
+		
+		labelResult = new Text(composite, SWT.FILL);
+		GridData gridDataLabel = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
+		gridDataLabel.widthHint = 300;
+		labelResult.setLayoutData(gridDataLabel);
+		labelResult.setEditable(false);
+		
+		
+		labelErrors = new Text(composite, SWT.FILL);
+		GridData gridDataLabelErors = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
+		gridDataLabelErors.widthHint = 300;
+		labelErrors.setLayoutData(gridDataLabelErors);
+		labelErrors.setEditable(false);
+		labelErrors.setText("");
+		
+	}
+
+
+
+	protected boolean validValues() {
+		
+		boolean valid = true;
+		
+		
+		if (
+				(textFilePath.getText().isEmpty() || textFilePath.getText().trim().length() < 1) ||
+				(textTextToSearch.getText().isEmpty() || textTextToSearch.getText().trim().length() < 1) ||
+				(textNewFilePath.getText().isEmpty() || textNewFilePath.getText().trim().length() < 1) ||
+				(searchMode == -1)
+				) {
+			valid = false;
+		} else {
+			if (!textNewFilePath.getText().substring(textNewFilePath.getText().length() -1).equals("\\")) {
+				textNewFilePath.setText(textNewFilePath.getText().concat("\\"));
+			}
+			if (!textFilePath.getText().substring(textFilePath.getText().length() -1).equals("\\")) {
+				textFilePath.setText(textFilePath.getText().concat("\\"));
+			}
+		}
+		
+		
+		
+		
+		return valid;
+	}
+
+
+
+	private void open() {
+		
+		shell.open();
+		while (!shell.isDisposed ()) {
+			if (!display.readAndDispatch ()) display.sleep ();
+		}
+		display.dispose ();
+		
+	}
+	
+	public static void main(String[] args) {
+		new Inicio();
+	}
+	
+
+}
